2ecb58f58e948ceb30baa6b58fb827a87148ee32
khanh <khanh.trieu@riviu.vn>
2022-06-04 00:22:30 +0700

    ss

--- web/source/nuxt.config.js
+++ web/source/nuxt.config.js
@@ -8,11 +8,11 @@ function resolve(dir) {
 const axios = require('axios');
 const messages = __importDefault(require("./lang/index.js"));
 
-const host = 'http://localhost:3000'
-const hostapi = 'http://10.39.93.112:1337'
+// const host = 'http://localhost:3000'
+// const hostapi = 'http://10.39.93.112:1337'
 
-// const host = 'http://avt.com.vn'
-// const hostapi = 'http://191.191.1.121:1337'
+const host = 'http://avt.com.vn'
+const hostapi = 'http://191.191.1.121:1337'
 
 const getSitemapsConfigurations = () => {
     let list = [{
